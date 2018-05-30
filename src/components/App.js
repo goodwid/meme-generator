0997b67d541dd9faa9c76775fb2729fa3026455e
David Goodwin <goodwid@gmail.com>
2018-05-29 22:15:15 -0700

    fixed errant bang

--- src/components/App.js
+++ src/components/App.js
@@ -2,17 +2,14 @@ import React, { Component } from 'react';
 
 export default class App extends Component {
   constructor(props) {
-    super(props);
-    this.state = {
-     
-    };
-    
+    super(props);    
   }
 
   render() {
   
     return (
       <main>
+        hello bob.
       </main> 
     );
   }
