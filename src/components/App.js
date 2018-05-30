92f66424973d2cc4d8239272a5187c090150b61a
David Goodwin <goodwid@gmail.com>
2018-05-30 00:50:31 -0700

    added header and footer inputs and change handler

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,51 @@
 import React, { Component } from 'react';
+import dom2image from 'dom-to-image';
+import fileSaver from 'file-saver';
+
 
 export default class App extends Component {
   constructor(props) {
-    super(props);    
+    super(props);
+    this.state = {
+      header: 'header',
+      footer: 'footer',
+    };
+    this.handleChange = this.handleChange.bind(this)
+    
+  }
+  handleChange(item) {
+    return ({ target }) => {
+      const state = {}
+      state[item] = target.value;
+      this.setState(state);
+    };
   }
 
   render() {
+    const { header, footer } = this.state;
   
     return (
       <main>
-        hello bob.
+        <h1>Welcome to David's meme generator!</h1>
+        header:
+        <br/>
+        <input
+          value={header}
+          onChange={this.handleChange('header')}
+        />
+        <br/>
+        <hr/>
+        footer:
+        <br/>
+        <input
+          value={footer}
+          onChange={this.handleChange('footer')}
+        />
+        <br/>
+        <hr/>
+        <div className="output">
+          {header}
+        </div>
       </main> 
     );
   }
